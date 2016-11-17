3d8021876283ecd9b002906732d56a0686dbc5e4
Diêggo Faustino <dieggofaustino@hotmail.com>
2016-11-17 16:10:16 -0200

    Change report field label

--- zup-painel/config/main.constants.js
+++ zup-painel/config/main.constants.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://localhost:3000',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'11',flowsEnabled:'true',pageTitle:'Fora da Escola Não Pode!',zendeskUrl:'https://buscaativaescolar.zendesk.com/',ckeditorPath:'assets/scripts/ckeditor/ckeditor.js',version:'1.2.0'})
+.constant('ENV', {name:'production',apiEndpoint:'http://localhost:3000',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'11',flowsEnabled:'true',pageTitle:'Fora da Escola Não Pode!',zendeskUrl:'https://buscaativaescolar.zendesk.com',ckeditorPath:'assets/scripts/ckeditor/ckeditor.js',version:'1.2.0'})
 
 ;
