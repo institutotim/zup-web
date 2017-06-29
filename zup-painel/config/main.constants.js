0393ccfbbd73b9c8d968be6dfbf48042c29c6775
Antonio Castanheira <antonio.augusto.castanheira@gmail.com>
2017-06-29 11:31:57 -0300

    Updating zup-web

--- zup-painel/config/main.constants.js
+++ zup-painel/config/main.constants.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://api:80',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'11',flowsEnabled:'true',pageTitle:'PAGE_TITLE',zendeskUrl:'https://zeladoriaurbana.zendesk.com/hc/pt-br',ckeditorPath:'assets/scripts/ckeditor/ckeditor.js',version:'1.2.0'})
+.constant('ENV', {name:'production',apiEndpoint:'http://zup.conexaobrasil.org:8282',mapLat:'-23.1001918',mapLng:'-48.9460738',mapZoom:'11',flowsEnabled:'',namespacesEnabled:'',pageTitle:'PAGE_TITLE',zendeskUrl:'https://zeladoriaurbana.zendesk.com/hc/pt-br',ckeditorPath:'assets/scripts/ckeditor/ckeditor.js',version:'1.3.4'})
 
 ;
